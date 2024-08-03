73610368f1f4a72143d5e147a73922364636152c
Kaushik Manivannan <manivannan.k@northeastern.edu>
2024-08-03 23:42:39 +0530

    Move Providers Into Separate Folder

---
+++ providers/ToastProvider.tsx
@@ -0,0 +1,29 @@
+"use client";
+
+import { ToastContainer } from "react-toastify";
+import 'react-toastify/dist/ReactToastify.css';
+
+interface ToastProviderProps {
+  children: React.ReactNode;
+}
+
+export default function ToastProvider({ children }: ToastProviderProps) {
+  return (
+    <>
+      {children}
+      <ToastContainer
+        stacked
+        position="bottom-center"
+        autoClose={3000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="light"
+      />
+    </>
+  );
+}
